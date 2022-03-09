2a7f96e80bd4f73ee9e556da6f8a5e66b6b754fd
Christina Phan <cphan4school@gmail.com>
2022-03-08 23:55:23 -0800

    Changed layout and styling of views

--- PhanCharacters/resources/views/characters/index.blade.php
+++ PhanCharacters/resources/views/characters/index.blade.php
@@ -1,27 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Characters</title>
-</head>
+@extends('layout')
+
+@section('content')
 <body>
-    <header>Characters</header>
-    <main>
-        @foreach ($characters as $character)
-            <section>
-                <figure>
-                    <img src="{{$character->img}}" alt="{{$character->name}}">
-                </figure>
-                <h1>{{ $character->name }}</h1>
-                <p>Status {{ $character->status }}</p>                    <p>Birthdate {{ $character->birthday }}</p>
-                <h2>Occupation</h2>
-                <ul>
-                    <li>{{ $character->occupation }}</li>  
-                </ul>  
-            </section>
-        @endforeach
+    <header class="text-center h1 mt-5">Breaking Bad Characters</header>
+    <main class="text-nowrap">
+        <div class="table-responsive mt-5">
+            <table class="table table-striped no-select">
+                <tr scope="row">
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Birthday</th>
+                </tr>
+                @foreach ($characters as $character)
+                <tr class="characters">
+                    <td>{{ $character->id }}</td>
+                    <td><a class="character-name" href="{{ route('characters.show', $character->id) }}">{{ $character->name }}</a></td>
+                    <td>{{ $character->status }}</td>
+                    <td>{{ $character->birthday }}</td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
     </main>
-</body>
-</html>
+        <script src=
+     "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js">
+    </script>
+   
+@endsection
