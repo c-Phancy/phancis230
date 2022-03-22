ff8cf75b9cca22d5036c17b8972300cb0d7538ac
Christina Phan <cphan4school@gmail.com>
2022-03-21 17:40:36 -0700

    Updated common layout files for Toast notifications

--- PhanCharacters/resources/views/characters/index.blade.php
+++ PhanCharacters/resources/views/characters/index.blade.php
@@ -1,30 +1,61 @@
 @extends('layout')
 
+@section('css')
+<link rel=stylesheet href={{ asset('css/index.css') }}>
+@endsection
+
 @section('content')
-<body>
-    <header class="text-center h1 mt-5">Breaking Bad Characters</header>
-    <main class="text-nowrap">
-        <div class="table-responsive mt-5">
-            <table class="table table-striped no-select">
-                <tr scope="row">
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Birthday</th>
-                </tr>
-                @foreach ($characters as $character)
-                <tr class="characters">
-                    <td>{{ $character->id }}</td>
-                    <td><a class="character-name" href="{{ route('characters.show', $character->id) }}">{{ $character->name }}</a></td>
-                    <td>{{ $character->status }}</td>
-                    <td>{{ $character->birthday }}</td>
-                </tr>
-                @endforeach
-            </table>
+<header class="text-center h1 mt-5">Breaking Bad Characters</header>
+@if (session()->get('success'))
+<div id="toast-wrapper">
+<div class="toast show mx-3">
+  <div class="toast-header d-flex justify-content-center">
+      <div style="width: 90%;" class="h4 mb-0">{{ session()->get('success') }}</div>
+    <button type="button" style="width: 5%;" class="btn" id="toast-button" data-bs-dismiss="toast">X</button>
+  </div>
+</div>
+</div>
+@endif
+<main class="text-nowrap">
+    {{ $characters->links('vendor.pagination.paginator') }}
+    <div class="table-responsive mb-4">
+        <div class="table d-table" id="table">
+            <div class="table-header d-table-row font-weight-bold">
+                <div class="d-table-cell px-3 my-1">ID</div>
+                <div class="d-table-cell px-3 my-1">Name</div>
+                <div class="d-table-cell px-3 my-1">Status</div>
+                <div class="d-table-cell px-3 my-1">Birthday</div>
+                <div class="d-table-cell px-3 my-1"><a class="btn border-0 text-light" id="create-button" href="{{ route('characters.create' ) }}">Create</a></div>
+            </div>
+            @foreach($characters as $character)
+                <div class="characters d-table-row">
+                    <div class="d-table-cell px-3">{{ $character->id }}</div>
+                    <div class="d-table-cell px-3"><a class="pointer character-name font-weight-bold"
+                            href="{{ route('characters.show', $character->id) }}">{{ $character->name }}</a>
+                    </div>
+                    <div class="px-3 d-table-cell">{{ $character->status }}</div>
+                    <div class="px-3 d-table-cell">{{ $character->birthday }}</div>
+                    <div class="px-3 d-table-cell">
+                        <form action="{{ route('characters.destroy', $character->id) }}"
+                            method="POST" onSubmit="return confirm('Are you sure you want to delete?');">
+                            @csrf
+                            @method('DELETE')
+                            <a class="pointer form-button edit-button btn border-0 text-light" href="{{ route('characters.edit', $character->id) }}">Edit</a>
+                            <button class="pointer form-button delete-button btn border-0 text-light"
+                                type="submit">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    </main>
-        <script src=
-     "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js">
-    </script>
-   
+    </div>
+    {{-- {{ $characters->links('pagination::bootstrap-5') }} --}}
+    {{ $characters->links('vendor.pagination.results') }}
+    {{ $characters->links('vendor.pagination.paginator') }}
+</main>
+@endsection
+@section('js')
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js">
+</script>
+<script src="{{ asset('js/index.js') }}"></script>
 @endsection
--- PhanCharacters/resources/views/characters/show.blade.php
+++ PhanCharacters/resources/views/characters/show.blade.php
@@ -1,56 +1,89 @@
+@php
+// $headers = get_headers($character->img);
+// for testing purposes until non-image URLs are blocked
+// $randomColor = sprintf('#%06X', mt_rand(0, 0xFFFFFF));
+@endphp
+
 @extends('layout')
 
 @section('title', $character->name)
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/show.css') }}">
+
+{{-- @if (strpos($headers['Content-Type'], 'image/') !== false) --}}
+<style>
+    body {
+        background: url({{ $character->img }}) center no-repeat;
+        background-size: cover;
+    }
+</style>
+{{-- @else
+<style>
+    body {
+        background-color: {{ $randomColor }};
+    }
+</style> --}}
+{{-- @endif --}}
+<link rel="stylesheet" href="{{ asset('css/show.css') }}" type="text/css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 @endsection
 @section('content')
-<body class="text-center" style="background: url({{ $character->img }}) center no-repeat; background-size: cover;">
-    <main class="my-5 mx-auto">
-        <div class="mx-0 mx-sm-4 pb-3">
-            <h1 class="pt-5 px-3">{{ $character->name }}</h1>
-            <p class="text-dark h3 font-weight-light font-italic mb-1">{{ $character->status }}</p>
-            <div id="image-modal" class="modal" tabindex="-1">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content border-0">
-                        <div class="d-flex justify-content-end p-1">
-                            <div>
-                        <button id="modal-button" type="button" class="border-0 bg-transparent d-flex justify-content-center align-items-center p-1" data-bs-dismiss="modal" aria-label="Close">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
-                        </button>
-                            </div>
+<main class="my-5 mx-auto">
+    <div class="mx-0 mx-sm-4 pb-3">
+        <h1 class="pt-5 px-3">{{ $character->name }}</h1>
+        <p class="text-dark h3 font-weight-light font-italic mb-1">{{ $character->status }}</p>
+        {{-- @if (strpos($headers['Content-Type'], 'image/') !== false) --}}
+        <div id="image-modal" class="modal" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content border-0">
+                    <div class="d-flex justify-content-end p-1">
+                        <div>
+                            <button id="modal-button" type="button"
+                                class="border-0 bg-transparent d-flex justify-content-center align-items-center p-1"
+                                data-bs-dismiss="modal" aria-label="Close">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-x-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                    <path
+                                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                </svg>
+                            </button>
                         </div>
-                        <div class="modal-header">
+                    </div>
+                    <div class="modal-header">
                         <div class="h1 modal-title mx-auto">{{ $character->name }}</div>
-                        </div>
-                        <div class="modal-body">
-                            <img class="w-100" src="{{ $character->img }}" alt="{{ $character->name }}">
-                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <img class="w-100" src="{{ $character->img }}" alt="{{ $character->name }}">
                     </div>
                 </div>
             </div>
-            <figure class="py-3 mb-1">
-                <button id="modal-button" type="button" class="btn w-50 p-0 border-none" data-bs-toggle="modal" data-bs-target="#image-modal">
-                    <img src="{{ $character->img }}" class="w-100" alt="{{ $character->name }}">
-                    <span id="tooltip">Click to expand</span>
-                </button>
-                <div id="image-instruction">Click to expand</div>
-            </figure>
-            <p class="h4 pb-3">Birthday<span class="h5 d-block font-italic font-weight-light pt-1">{{ $character->birthday }}</span></p>
-            <p class="h4 pb-3 px-4">Occupation<span class="h5 d-block font-italic font-weight-light pt-1">{{ $character->occupation }}</span></p>
-        </div>   
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script>
-        const tooltip = document.getElementById('tooltip');
-        window.onmousemove = function(e) {
-            let x = e.clientX, y = e.clientY;
-            tooltip.style.top = (y + 15) + 'px';
-            tooltip.style.left = (x + 15) + 'px';
-        }
-    </script>
-    @endsection
+        </div>
+        <figure class="py-3 mb-1">
+            <button id="modal-button" type="button" class="btn w-50 p-0 border-none" data-bs-toggle="modal"
+                data-bs-target="#image-modal">
+                <img src="{{ $character->img }}" class="w-100" alt="{{ $character->name }}">
+                <span id="tooltip">Click to expand</span>
+            </button>
+            <div id="image-instruction">Click to expand</div>
+        </figure>
+        {{-- @endif --}}
+        <p class="h4 pb-3">Birthday<span
+                class="h5 d-block font-italic font-weight-light pt-1">{{ $character->birthday }}</span></p>
+        <p class="h4 pb-3 px-4">Occupation<span
+                class="h5 d-block font-italic font-weight-light pt-1">{{ $character->occupation }}</span></p>
+    </div>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<script>
+    const tooltip = document.getElementById('tooltip');
+    window.onmousemove = function (e) {
+        let x = e.clientX,
+            y = e.clientY;
+        tooltip.style.top = (y + 15) + 'px';
+        tooltip.style.left = (x + 15) + 'px';
+    }
+
+</script>
+@endsection
